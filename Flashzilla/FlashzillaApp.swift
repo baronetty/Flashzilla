220c8b653b4a41546080b757123665eb320122d8
Leo Barnett <leo.e.barnett@icloud.com>
2024-02-22 16:55:28 +0100

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Leo  on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
